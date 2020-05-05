e89fa7370d3a652a5712e92a5f9403cc53fa3b65
jgrimes@relias.com <jgrimes@relias.com>
2020-05-05 18:38:23 -0400

    Update add-cases and cases-stat components, add yarn support - Update html, ts, and scss files in add-cases component - Update html, ts, and scss files in cases-stat component - Add yarn support

--- src/app/components/add-cases/add-cases.component.ts
+++ src/app/components/add-cases/add-cases.component.ts
@@ -1,4 +1,18 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+
+import {ErrorStateMatcher} from '@angular/material/core';
+
+import {ApiService} from '../../services/api.service';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-add-cases',
@@ -7,10 +21,45 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AddCasesComponent implements OnInit {
 
-  constructor() {
+  casesForm: FormGroup;
+  name = '';
+  gender = '';
+  age: number = null;
+  address = '';
+  city = '';
+  country = '';
+  status = '';
+  statusList = ['Positive', 'Dead', 'Recovered'];
+  genderList = ['Male', 'Female'];
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.casesForm = this.formBuilder.group({
+      name: [null, Validators.required],
+      gender: [null, Validators.required],
+      age: [null, Validators.required],
+      address: [null, Validators.required],
+      city: [null, Validators.required],
+      country: [null, Validators.required],
+      status: [null, Validators.required]
+    });
+  }
+
+  onFormSubmit() {
+    this.isLoadingResults = true;
+    this.api.addCase(this.casesForm.value)
+      .subscribe((res: any) => {
+        const id = res._id;
+        this.isLoadingResults = false;
+        this.router.navigate(['/cases-detail', id]);
+      }, (err: any) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
--- src/app/components/cases-stat/cases-stat.component.ts
+++ src/app/components/cases-stat/cases-stat.component.ts
@@ -1,5 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 
+import {ChartDataSets, ChartOptions, ChartType} from 'chart.js';
+import {Label} from 'ng2-charts';
+
+import {ApiService} from '../../services/api.service';
+import {Statistic} from '../../models/statistic.model';
+
 @Component({
   selector: 'app-cases-stat',
   templateUrl: './cases-stat.component.html',
@@ -7,10 +13,55 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CasesStatComponent implements OnInit {
 
-  constructor() {
+  stats: Statistic[] = [];
+  label = 'Positive';
+  isLoadingResults = true;
+  barChartOptions: ChartOptions = {
+    responsive: true
+  };
+  barChartLabels: Label[] = [];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins = [];
+  barChartData: ChartDataSets[] = [{data: [], backgroundColor: [], label: this.label}];
+
+  constructor(private api: ApiService) {
   }
 
   ngOnInit(): void {
+    this.getStatistic(this.label);
+  }
+
+  changeStatus() {
+    this.isLoadingResults = true;
+    this.getStatistic(this.label);
+  }
+
+  getStatistic(status: string) {
+    this.barChartData = [{data: [], backgroundColor: [], label: this.label}];
+    this.barChartLabels = [];
+    this.api.getStatistic(status)
+      .subscribe((res: any) => {
+        this.stats = res;
+        const chartData: number[] = [];
+        const chartColor: string[] = [];
+        this.stats.forEach((stat) => {
+          this.barChartLabels.push(stat._id.date);
+          chartData.push(stat.count);
+          if (this.label === 'Positive') {
+            chartColor.push('rgba(255, 165, 0, 0.5)');
+          } else if (this.label === 'Dead') {
+            chartColor.push('rgba(255, 0, 0, 0.5)');
+          } else {
+            chartColor.push('rgba(0, 255, 0, 0.5)');
+          }
+        });
+        this.barChartData = [{data: chartData, backgroundColor: chartColor, label: this.label}];
+        this.isLoadingResults = false;
+      }, err => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
