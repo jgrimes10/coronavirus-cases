cd30116afb97911accee5ea8904f524b11537664
Justin Grimes <jgrimes@relias.com>
2020-05-05 16:06:56 -0400

    Add components & setup routing - Add add-cases, cases, cases-details, cases-stat, and edit-cases components. - Set up the routing to the new components as well as the empty path to redirect to cases.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,44 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+// Components
+import {CasesComponent} from "./components/cases/cases.component";
+import {CasesDetailsComponent} from "./components/cases-details/cases-details.component";
+import {CasesStatComponent} from "./components/cases-stat/cases-stat.component";
+import {AddCasesComponent} from "./components/add-cases/add-cases.component";
+import {EditCasesComponent} from "./components/edit-cases/edit-cases.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cases',
+    component: CasesComponent,
+    data: {title: 'List of Cases'}
+  },
+  {
+    path: 'cases-details/:id',
+    component: CasesDetailsComponent,
+    data: {title: 'Cases Details'}
+  },
+  {
+    path: 'cases-stat',
+    component: CasesStatComponent,
+    data: {title: 'Cases Statistic'}
+  },
+  {
+    path: 'add-cases',
+    component: AddCasesComponent,
+    data: {title: 'Add Cases'}
+  },
+  {
+    path: 'edit-cases/:id',
+    component: EditCasesComponent,
+    data: {title: 'Edit Cases'}
+  },
+  {
+    path: '',
+    redirectTo: '/cases',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CasesComponent} from './components/cases/cases.component';
+import {CasesDetailsComponent} from './components/cases-details/cases-details.component';
+import {AddCasesComponent} from './components/add-cases/add-cases.component';
+import {EditCasesComponent} from './components/edit-cases/edit-cases.component';
+import {CasesStatComponent} from './components/cases-stat/cases-stat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CasesComponent,
+    CasesDetailsComponent,
+    AddCasesComponent,
+    EditCasesComponent,
+    CasesStatComponent
   ],
   imports: [
     BrowserModule,
