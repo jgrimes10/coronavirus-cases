0e419ccd99946b58e01f12a070971ea9153912e9
Justin Grimes <jgrimes@relias.com>
2020-05-05 17:16:22 -0400

    Update cases details component - Update html, scss, and ts of the cases-details component

--- src/app/components/cases-details/cases-details.component.ts
+++ src/app/components/cases-details/cases-details.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+import {ApiService} from '../../services/api.service';
+import {Case} from '../../models/case.model';
 
 @Component({
   selector: 'app-cases-details',
@@ -7,10 +11,45 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CasesDetailsComponent implements OnInit {
 
-  constructor() {
+  cases: Case = {
+    _id: '',
+    name: '',
+    gender: '',
+    age: null,
+    address: '',
+    city: '',
+    country: '',
+    status: '',
+    updated: null
+  };
+  isLoadingResults = true;
+
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) {
   }
 
   ngOnInit(): void {
+    this.getCasesDetails(this.route.snapshot.params.id);
+  }
+
+  getCasesDetails(id: string) {
+    this.api.getCaseById(id)
+      .subscribe((data: any) => {
+        this.cases = data;
+        console.log(this.cases);
+        this.isLoadingResults = false;
+      });
+  }
+
+  deleteCases(id: any) {
+    this.isLoadingResults = true;
+    this.api.deleteCase(id)
+      .subscribe(res => {
+        this.isLoadingResults = false;
+        this.router.navigate(['/cases']);
+      }, (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
   }
 
 }
